d479bfa6bf31aeb5438eb2667436a3ca8fde75e5
Yan <yancolabs@gmail.com>
2023-09-17 15:07:33 +0600

    ipopup close and open

--- static/static/scripts/baseof.js
+++ static/static/scripts/baseof.js
@@ -1,9 +1,13 @@
 let ipopup = document.getElementById("obj-baseof-ipopup");
 let ipopupImage = document.getElementById("img-baseof-ipopup-image");
 
-for (let img of document.getElementsByTagName("img")) {
+for (let img of document.querySelectorAll("img:not(#img-baseof-ipopup-image)")) {
     img.addEventListener("click", () => {
-        ipopup.style.display = "none";
-        ipopupImage.src = self.src;
+        ipopup.style.display = "block";
+        ipopupImage.src = img.src;
     });
+}
+
+function closeIPopup() {
+    ipopup.style.display = "none";
 }
