49274e1d18616f2c46b7b9b5d1fd58ac21edcd98
Yan <yancolabs@gmail.com>
2023-11-12 20:15:37 +0600

    hugo call

--- public/static/scripts/baseof.js
+++ public/static/scripts/baseof.js
@@ -3,7 +3,7 @@
 let ipopup = document.getElementById("obj-baseof-ipopup");
 let ipopupImage = document.getElementById("img-baseof-ipopup-image");
 
-for (let img of document.querySelectorAll("img:not(#img-baseof-ipopup-image)")) {
+for (let img of document.querySelectorAll("img:not(#img-baseof-ipopup-image):not(.tooltip-img)")) {
     img.addEventListener("click", () => {
         ipopup.style.opacity = 1;
         ipopup.style.pointerEvents = "all";
